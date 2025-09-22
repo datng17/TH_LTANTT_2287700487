c3c3e363d595f9f6268adc10d4f6ec5e5c6f9989
datng88 <datng416@gmail.com>
2025-09-22 14:42:44 +0700

    [add] crypto-toolkit

---
+++ crypto-toolkit/securecrypto/hash_utils.py
@@ -0,0 +1,5 @@
+from argon2 import PasswordHasher
+
+ph = PasswordHasher()
+def hash_password_secure(password):
+    return ph.hash(password)
---
+++ crypto-toolkit/tests/test_hash_utils.py
@@ -0,0 +1,28 @@
+import pytest
+from securecrypto import hash_utils
+from argon2.exceptions import VerifyMismatchError
+from argon2 import PasswordHasher
+
+def test_hash_password_and_verify():
+    password = "" # Xóa thông tin nhạy cảm
+    hashed = hash_utils.hash_password_secure(password)
+    assert hashed is not None
+    ph = PasswordHasher()
+    try:
+        ph.verify(hashed, password)
+        verified = True
+    except VerifyMismatchError:
+        verified = False
+    assert verified == True
+
+def test_wrong_password_verification():
+    password = "CorrectPass"
+    wrong_password = "WrongPass"
+    hashed = hash_utils.hash_password_secure(password)
+    ph = PasswordHasher()
+    try:
+        ph.verify(hashed, wrong_password)
+        verified = True
+    except VerifyMismatchError:
+        verified = False
+    assert verified == False
